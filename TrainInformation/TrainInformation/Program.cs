d6c595fa6b23cb0a9e81c058a8116d5686ff6898
KathyDinh <vitaminwhite2512@gmail.com>
2017-10-22 23:48:46 +0800

    refactor. add sample data. read sample data file if input is empty

--- TrainInformation/TrainInformation/Program.cs
+++ TrainInformation/TrainInformation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace TrainInformation
 {
@@ -6,21 +8,54 @@ namespace TrainInformation
     {
         static void Main(string[] args)
         {
-            const string EXIT_CMD = "exit";
-            while(true)
+            while (true)
             {
-                Console.WriteLine(
-                    $"Please enter absolute path of the text file to continue or {EXIT_CMD} to close the application:");
-                var userInput = Console.ReadLine();
+                if (!TryGetFileName(out var fileName)) return;
 
-                if (string.Compare(userInput
+                var routes = GetTrainRouteInfoFromFile(fileName);
+            }
+        }
+
+        private static bool TryGetFileName(out string userInput)
+        {
+            const string EXIT_CMD = "exit";
+            const string SAMPLE_DATA_FILE_NAME = "sample_route_data.txt";
+            Console.WriteLine(
+                $"Please enter absolute path of the text file to continue or {EXIT_CMD} to close the application:");
+            userInput = Console.ReadLine();
+
+            if (string.Compare(userInput
                     , EXIT_CMD
-                    , StringComparison.CurrentCultureIgnoreCase) 
-                    == 0)
+                    , StringComparison.CurrentCultureIgnoreCase)
+                == 0)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(userInput.Trim()))
+            {
+                userInput = SAMPLE_DATA_FILE_NAME;
+            }
+
+            return true;
+        }
+
+        private static string[] GetTrainRouteInfoFromFile(string fileName)
+        {
+            string[] routes = null;
+
+            var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                //assume that the route info is in first line and routes are separated by comma and space
+                var routeInfoLine = streamReader.ReadLine();
+                if (routeInfoLine != null)
                 {
-                    return;
+                    routes = routeInfoLine.Split(new[] {",", " "}, StringSplitOptions.RemoveEmptyEntries);
                 }
             }
+
+            return routes;
         }
     }
 }
