acb5d8a06b592340cf9b0686b1a8a03007856ec7
KathyDinh <vitaminwhite2512@gmail.com>
2017-10-22 23:17:51 +0800

    get user input for file name

--- TrainInformation/TrainInformation/Program.cs
+++ TrainInformation/TrainInformation/Program.cs
@@ -6,7 +6,21 @@ namespace TrainInformation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string EXIT_CMD = "exit";
+            while(true)
+            {
+                Console.WriteLine(
+                    $"Please enter absolute path of the text file to continue or {EXIT_CMD} to close the application:");
+                var userInput = Console.ReadLine();
+
+                if (string.Compare(userInput
+                    , EXIT_CMD
+                    , StringComparison.CurrentCultureIgnoreCase) 
+                    == 0)
+                {
+                    return;
+                }
+            }
         }
     }
 }
