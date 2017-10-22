29fce1c9650a62353199bf0e6f32802b26d81157
KathyDinh <vitaminwhite2512@gmail.com>
2017-10-23 00:43:08 +0800

    create a console app in .NET framework instead of .NET Core to enable Nunit testing. Add unit test for getting routes

---
+++ TrainInformation/TrainInformation.Test/ProgramTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TrainInformation;
+
+namespace TrainInformation.Test
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void GetTrainRouteInfoFromFile_ShouldReturnCorrectRouteInfo()
+        {
+            var fileName = TestContext.CurrentContext.TestDirectory + @"/sample_route_data.txt";
+            var expected = new[]
+            {
+                "AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7"
+            };
+
+            var actual = Program.GetTrainRouteInfoFromFile(fileName);
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+}
